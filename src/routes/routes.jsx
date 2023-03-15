956b672a90d0699520f935c8542a59cf077742ff
arishuy <huybui19898@gmail.com>
2023-03-15 16:34:16 +0700

    update

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import ChangePassword from '../components/ChangePassword';
 import DashBoardPage from '../pages/DashBoardPage';
 import Login from '../pages/Login';
 import ProfilePage from '../pages/ProfilePage';
@@ -13,7 +12,6 @@ const routes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/test" element={<ChangePassword />} />
     </Routes>
   )
 }
