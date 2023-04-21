348e0dd91e62e43295f5596a765f6c8dd3b3b067
arishuy <huybui19898@gmail.com>
2023-04-21 15:56:04 +0700

    fix bug giao dien:

--- src/pages/DashBoardPage.jsx
+++ src/pages/DashBoardPage.jsx
@@ -13,13 +13,13 @@ const DashBoardPage = () => {
     // </div>
     <Layout style={{ minHeight: '100vh' }}>
       <Menu />
-      <Layout className="site-layout">
+      <Layout className="site-layout" style={{maxHeight: '100vh'}}>
         <Header style={{ padding: 0, background: '#EFEFEF', height: '55px',display: 'flex', alignItems: 'center', position: 'relative'}}>
           <Headerbar />
         </Header>
         <Content style={{ background: '#E7E7E7' }}>
           <div style={{ padding: 24, minHeight: 360 }}>
-            Dữ liệu trống
+            <DashBoard />
           </div>
         </Content>
         <Footer style={{ textAlign: 'center',background: '#EFEFEF' }}>PBL5 IoT</Footer>
