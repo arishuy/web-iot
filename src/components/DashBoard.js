54858182b1edcb33b6f378107ad4fe23f2133e98
arishuy <huybui19898@gmail.com>
2023-04-22 11:37:43 +0700

    add get video from server

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -6,12 +6,15 @@ import Homepage from './Homepage';
 
 const DashBoard = () => {
   const token = localStorage.getItem("token");
+  console.log(process.env.REACT_APP_PROXY_URL)
+  const url = `${process.env.REACT_APP_PROXY_URL}/video_feed/jetsonnano`
   return (
     <div className="dashboard">
       { token ? 
       <div>
       <h1 className="header_dashboard">Cam: </h1>
-      <FaceDetector />
+      {/* <FaceDetector /> */}
+      <img src={url} alt="Camera stream 1" />
       </div>
       :
       <Homepage />
